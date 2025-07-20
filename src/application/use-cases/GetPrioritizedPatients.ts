e04ec5b0644abd5ad33dbc166221c3a3fd6526ce
igor.quintao <igor.quintao@edetek.com>
2025-07-20 13:36:55 -0300

    Use case: GetPrioritizedPatients

---
+++ src/application/use-cases/GetPrioritizedPatients.ts
@@ -0,0 +1,41 @@
+import { PatientRepository } from '../../domain/repositories/PatientRepository';
+import { ScoringService } from '../../domain/services/ScoringService';
+import { Location, ScoredPatient } from '../../domain/entities/Patient';
+import { GetPrioritizedPatientsRequestDTO, GetPrioritizedPatientsResponseDTO, PrioritizedPatientDTO } from '../dto/PrioritizedPatientDTO';
+
+export class GetPrioritizedPatients {
+  constructor(
+    private readonly patientRepository: PatientRepository,
+    private readonly scoringService: ScoringService
+  ) {}
+
+  async execute(request: GetPrioritizedPatientsRequestDTO): Promise<GetPrioritizedPatientsResponseDTO> {
+    const facilityLocation: Location = {
+      latitude: request.facility.location.latitude,
+      longitude: request.facility.location.longitude,
+    };
+
+    const patients = await this.patientRepository.findAll();
+
+    const scoredPatients: ScoredPatient[] = patients.map(patient =>
+      this.scoringService.calculateScore(patient, facilityLocation)
+    );
+
+    scoredPatients.sort((a, b) => b.score - a.score);
+
+    const topPatients = scoredPatients.slice(0, 10);
+
+    const prioritizedPatients: PrioritizedPatientDTO[] = topPatients.map(patient => ({
+      id: patient.id,
+      name: patient.name,
+      score: patient.score,
+      distance: patient.distance || 0,
+      demographicScore: patient.demographicScore,
+      behavioralScore: patient.behavioralScore,
+    }));
+
+    return {
+      patients: prioritizedPatients,
+    };
+  }
+}
